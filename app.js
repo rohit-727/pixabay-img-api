a3390ee245460cea78fad341eaec810b23cbd74c
Rohit A S <145858319+rohit-a4s5@users.noreply.github.com>
2024-08-09 13:41:55 +0530

    Initial code push

---
+++ app.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const express = require('express');
+const request = require('request');
+
+// Loading dotenv file
+require('dotenv').config();
+
+const app = express();
+app.use(express.json())
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log("Server listening on port:", PORT);
+});
+
+app.get("/:query", async (req, res) => {
+    axios.get(`https://pixabay.com/api/?key=${process.env.API_KEY}&q=${req.params.query}`)
+    .then((response) => {
+        if(response.data.hits[0]){
+            const image = response.data.hits[0].largeImageURL;
+            request({ url: image, encoding: null }, (err, resp, buffer) => {
+                if (err) {
+                    console.error('Error fetching image:', err);
+                    res.status(500).send('Error fetching image');
+                } else if (resp.statusCode === 200) {
+                    res.set('Content-Type', 'image/jpeg');
+                    res.send(resp.body);
+                } else {
+                    console.error('Error, status code:', resp.statusCode);
+                    res.status(500).send('Unhandled status code');
+                }
+            })
+        }
+        else {
+            res.status(404).send('Image not found.')
+        }
+    });
+
+});
+
+
